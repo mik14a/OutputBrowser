80f138ed5bb1a8280b7a55a126fb1d199a8f008e
mik14a <mik14a@gmail.com>
2024-10-12 20:39:45 +0900

    Enhance image output functionality with context menu and metadata display

--- OutputBrowser/Pages/OutputPage.xaml.cs
+++ OutputBrowser/Pages/OutputPage.xaml.cs
@@ -1,8 +1,15 @@
+using System;
 using System.Collections.ObjectModel;
 using System.IO;
+using System.Threading.Tasks;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 using Microsoft.UI.Xaml.Controls;
 using OutputBrowser.ViewModels;
+using Windows.ApplicationModel.DataTransfer;
+using Windows.Storage;
+using Windows.Storage.Streams;
+using Windows.System;
 
 namespace OutputBrowser.Pages
 {
@@ -14,7 +21,7 @@ namespace OutputBrowser.Pages
     {
         public ObservableCollection<OutputViewModel> Outputs { get; } = [];
 
-        [ObservableProperty] string _path = null;
+        [ObservableProperty] string _imagePath = null;
 
         public OutputPage() {
             _watcher.Created += WatcherEvent;
@@ -25,8 +32,34 @@ namespace OutputBrowser.Pages
             DataContext = this;
         }
 
-        partial void OnPathChanged(string value) {
-            if (!string.IsNullOrWhiteSpace(value) && System.IO.Directory.Exists(value)) {
+        [RelayCommand]
+        static async Task OpenFolderAsync(OutputViewModel output) {
+            var imageFile = await StorageFile.GetFileFromPathAsync(output.ImagePath);
+            var imageFolder = await StorageFolder.GetFolderFromPathAsync(Path.GetDirectoryName(output.ImagePath));
+            var folderLauncherOptions = new FolderLauncherOptions();
+            folderLauncherOptions.ItemsToSelect.Add(imageFile);
+            await Launcher.LaunchFolderAsync(imageFolder, folderLauncherOptions);
+        }
+
+        [RelayCommand]
+        static async Task CopyAsync(OutputViewModel output) {
+            var imageFile = await StorageFile.GetFileFromPathAsync(output.ImagePath);
+            var dataPackage = new DataPackage();
+            dataPackage.SetStorageItems([imageFile]);
+            Clipboard.SetContent(dataPackage);
+        }
+
+        [RelayCommand]
+        static async Task CopyImageAsync(OutputViewModel output) {
+            var imageFile = await StorageFile.GetFileFromPathAsync(output.ImagePath);
+            var stream = await imageFile.OpenReadAsync();
+            var dataPackage = new DataPackage();
+            dataPackage.SetBitmap(RandomAccessStreamReference.CreateFromStream(stream));
+            Clipboard.SetContent(dataPackage);
+        }
+
+        partial void OnImagePathChanged(string value) {
+            if (!string.IsNullOrWhiteSpace(value) && Directory.Exists(value)) {
                 _watcher.Path = value;
                 _watcher.EnableRaisingEvents = true;
                 _watcher.IncludeSubdirectories = true;
@@ -35,7 +68,7 @@ namespace OutputBrowser.Pages
 
         void WatcherEvent(object sender, FileSystemEventArgs e) {
             var fullPath = e.FullPath;
-            if (System.IO.Path.GetExtension(fullPath) == ".png") {
+            if (Path.GetExtension(fullPath) == ".png") {
                 DispatcherQueue.TryEnqueue(() => Outputs.Add(new OutputViewModel(fullPath)));
             }
         }
--- OutputBrowser/ViewModels/OutputViewModel.cs
+++ OutputBrowser/ViewModels/OutputViewModel.cs
@@ -3,14 +3,17 @@ using System.IO;
 using CommunityToolkit.Mvvm.ComponentModel;
 using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Media.Imaging;
+using Windows.Graphics.Imaging;
 using Windows.Storage;
 
 namespace OutputBrowser.ViewModels
 {
     public partial class OutputViewModel : ObservableRecipient
     {
-        [ObservableProperty] string _imagePath;
+        [ObservableProperty] string _imagePath = null;
+        [ObservableProperty] bool _visibleContactInfo = false;
         public ImageSource Image => _image ??= CreateImage();
+        public string ContactInfo => _contactInfo ?? GetContactInfo();
 
         public OutputViewModel(string path) {
             _imagePath = path;
@@ -18,18 +21,35 @@ namespace OutputBrowser.ViewModels
 
         BitmapSource CreateImage() {
             return !string.IsNullOrEmpty(ImagePath) && File.Exists(ImagePath)
-                ? CreateBitmapImage(ImagePath)
+                ? CreateImage(ImagePath)
                 : null;
 
-            static BitmapImage CreateBitmapImage(string path) {
+            static BitmapImage CreateImage(string path) {
                 var bitmap = new BitmapImage();
                 var file = StorageFile.GetFileFromPathAsync(path).GetAwaiter().GetResult();
-                var stream = file.OpenReadAsync().GetAwaiter().GetResult();
+                using var stream = file.OpenAsync(FileAccessMode.Read).GetAwaiter().GetResult();
                 bitmap.SetSource(stream);
                 return bitmap;
             }
         }
 
+        string GetContactInfo() {
+            return !string.IsNullOrEmpty(ImagePath) && File.Exists(ImagePath)
+                ? GetContactInfo(ImagePath)
+                : null;
+
+            static string GetContactInfo(string path) {
+                var file = StorageFile.GetFileFromPathAsync(path).GetAwaiter().GetResult();
+                using var stream = file.OpenAsync(FileAccessMode.Read).GetAwaiter().GetResult();
+                var decoder = BitmapDecoder.CreateAsync(stream).GetAwaiter().GetResult();
+                var retrieveProperties = decoder.BitmapProperties.GetPropertiesAsync(["/tEXt/{str=parameters}"]).GetAwaiter().GetResult();
+                return retrieveProperties.TryGetValue("/tEXt/{str=parameters}", out var parameters)
+                    ? (string)parameters.Value
+                    : null;
+            }
+        }
+
         ImageSource _image = null;
+        readonly string _contactInfo = null;
     }
 }
